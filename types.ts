18d4a4a805010e5cdfabab8012c83cc4239b19f1
JordanLandry <jordanlandry2001@gmail.com>
2023-12-17 18:31:52 -0500

    Added achievement type

--- types.ts
+++ types.ts
@@ -8,4 +8,11 @@ declare global {
     username: string;
     videoUri: string;
   };
+
+  type Achievement = {
+    id: string;
+    title: string;
+    description: string;
+    icon: string;
+  } & ({ completed: true; dateCompleted: number } | { completed: false });
 }
