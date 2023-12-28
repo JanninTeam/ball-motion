4405ddf22171e1aba434ee83d69a30e18d0b139d
JordanLandry <jordanlandry2001@gmail.com>
2023-12-27 22:33:14 -0500

    change: added settings context to App.tsx

--- App.tsx
+++ App.tsx
@@ -2,8 +2,18 @@ import { useFonts } from 'expo-font';
 import { StatusBar } from 'react-native';
 import AppNavigator from './src/Navigation';
 import LoadingPage from './src/pages/LoadingPage';
+import { createContext, useState } from 'react';
+
+type Settings = any; // TODO: Add type
+export const SettingsContext = createContext({
+  units: 'metric',
+  settings: {},
+  setSettings: (settings: Settings) => {},
+});
 
 export default function App() {
+  const [settings, setSettings] = useState<Settings>({});
+
   const [fontsLoaded] = useFonts({
     'Jost-Regular': require('./assets/fonts/Jost-Regular.ttf'),
     'Jost-SemiBold': require('./assets/fonts/Jost-SemiBold.ttf'),
@@ -14,8 +24,12 @@ export default function App() {
 
   return (
     <>
-      <StatusBar />
-      <AppNavigator />
+      <SettingsContext.Provider
+        value={{ settings, setSettings, units: 'metric' }}
+      >
+        <StatusBar />
+        <AppNavigator />
+      </SettingsContext.Provider>
     </>
   );
 }
