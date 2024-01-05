5449d9c259ddaa9314a3136437a8a0fb19391185
JordanLandry <jordanlandry2001@gmail.com>
2024-01-04 23:32:21 -0500

    update: made units dynamic in previousRunsPage

--- src/pages/PreviousRunsPage.tsx
+++ src/pages/PreviousRunsPage.tsx
@@ -3,20 +3,37 @@ import Page from '../components/Page';
 import { previousRuns } from '../../sampleData/previousRuns';
 import BaseText from '../components/BaseText';
 import theme from '../globals/globalStyles';
+import { formatDate } from '../util/formatDate';
+import { SettingsContext } from '../../App';
+import { useContext } from 'react';
+import getConvertedUnits from '../util/getConvertedUnits';
+import { Sport } from '../constants/units';
 
 export default function PreviousRunsPage() {
+  const { settings } = useContext(SettingsContext);
+
+  const getValue = (run: Activity) => {
+    const { value, unit } = getConvertedUnits({
+      value: run.speed,
+      settings,
+      sport: run.sport,
+      unitType: 'speed', // TODO: Add distance
+    });
+
+    return `${value.toFixed(1)} ${unit}`;
+  };
+
   return (
     <Page>
       <ScrollView>
-        {previousRuns.map((run) => {
-          return (
-            <View key={run.id} style={styles.run}>
-              <BaseText>{run.username}</BaseText>
-              <BaseText>{run.speed}</BaseText>
-              <BaseText>{run.date}</BaseText>
-            </View>
-          );
-        })}
+        {previousRuns.map((run) => (
+          <View key={run.id} style={styles.run}>
+            <BaseText>{run.username}</BaseText>
+            <BaseText>{run.sport}</BaseText>
+            <BaseText>{getValue(run)}</BaseText>
+            <BaseText>{formatDate(run.date)}</BaseText>
+          </View>
+        ))}
       </ScrollView>
     </Page>
   );
